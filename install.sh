6c5ec54a9f5a4d79c2883c2f15be6d2bfa9f45fa
Gorgon <gorgonsocial@proton.me>
2025-07-27 19:47:30 +0200

    Fixed issue with cd

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 
 # Installing needed dependencies
 echo "Installing git, base-devel..."
-sudo pacman -Syu --needed --noconfirm wget git base-devel
+sudo pacman -Syu --needed --noconfirm git base-devel
 echo "Needed dependencies installed succesfully!"
 
 # Install needed packages
@@ -15,7 +15,7 @@ cd ~/paru-temp
 makepkg -si
 echo "Paru installed succesfully!"
 rm -rf ~/paru-temp
-cd dots
+cd ~/dots
 
 # Installing software dependencies
 # AwesomeWM
@@ -89,12 +89,14 @@ echo "Successfully cloned wallpapers!"
 # Copy all .config and .local files to correct folders
 # Rofi theme
 echo "Installing Rofi theme..."
+cd ~/dots
 mkdir -p ~/.local/share/rofi/themes
 cp .local/share/rofi/themes/nord.rasi ~/.local/share/rofi/themes
 echo "Rofi theme installed successfully!"
 
 # Fish config
 echo "Installing Fish config"
+cd ~/dots
 mkdir -p ~/.config/fish
 cp -r .config/fish/* ~/.config/fish
 read -p "It's recommended to download mesloNF fonts for compatibility reasons. Download now? (Y/N) " answer
@@ -102,6 +104,7 @@ if [[ "$answer" =~ ^[Yy]$ ]]; then
 	echo "Downloading and setting up mesloNF..."
 	mkdir ~/fonts-temp
 	cd ~/fonts-temp
+	paru -S --noconfirm --neded wget
 	wget "https://github.com/IlanCosman/tide/blob/assets/fonts/mesloLGS_NF_regular.ttf?raw=true"
 	wget "https://github.com/IlanCosman/tide/blob/assets/fonts/mesloLGS_NF_bold.ttf?raw=true"
 	wget "https://github.com/IlanCosman/tide/blob/assets/fonts/mesloLGS_NF_italic.ttf?raw=true"
@@ -124,12 +127,14 @@ echo "Fish config installed successfully!"
 
 # Polybar config
 echo "Installing Polybar config..."
+cd ~/dots
 mkdir -p ~/.config/polybar
 cp .config/polybar/config ~/.config/polybar/config
 echo "Polybar config installed successfully!"
 
 # Awesome config
 echo "Installing AwesomeWM config..."
+cd ~/dots
 mkdir -p ~/.config/awesome/
 cp .config/awesome/rc.lua ~/.config/awesome/rc.lua
 cp .config/awesome/autorun.sh ~/.config/awesome/
@@ -138,12 +143,14 @@ echo "Awesome config installed successfully!"
 
 # NVIM config
 echo "Installing NVIM config..."
+cd ~/dots
 mkdir -p ~/.config/nvim
 cp -r .config/nvim/ ~/.config/nvim/
 echo "NVIM config installed successfully!"
 
 # Alacritty config
 echo "Installing Alacritty config..."
+cd ~/dots
 mkdir -p ~/.config/alacritty
 cp .config/alacritty/alacritty.toml ~/.config/alacritty
 echo "Alacritty config successfully installed!"
